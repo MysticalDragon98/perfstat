708e159651e6acc60a13681c222b3690abbd8187
CamiloTDex <ctorres@dexfreight.io>
2024-05-25 14:51:39 -0500

    Update

--- lib/modules/systemd/getSystemdServiceStatus.ts
+++ lib/modules/systemd/getSystemdServiceStatus.ts
@@ -1,5 +1,5 @@
 import executeShellCommand from "../../../plugins/shell/lib/modules/executeShellCommand";
 
 export default function getSystemdServiceStatus (serviceId: string) {
-    return await executeShellCommand("sudo", ["systemctl", "status", serviceId]);
+    return executeShellCommand("sudo", ["systemctl", "status", serviceId]);
 }
