8c4d08a2b9dc39abe393d565f6a4f5ae3f538c31
CamiloTDex <ctorres@dexfreight.io>
2024-05-25 15:05:29 -0500

    Update

--- plugins/shell/lib/modules/executeShellCommand.ts
+++ plugins/shell/lib/modules/executeShellCommand.ts
@@ -4,5 +4,10 @@ import ShellCommandOutput from "../classes/ShellCommandOutput";
 export default function executeShellCommand (command: string, args: string[]){
     const child = spawn(command, args);
 
+    child.on('error', (err) => {
+        console.error(`Error running command: ${command} ${args.join(" ")}`);
+        console.error(err);
+    });
+
     return new ShellCommandOutput(child);
 }
